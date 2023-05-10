4f8e6e9c151723bd75d1a1bfbe4b3b6a22c87077
Zach <zachlandis91@gmail.com>
2023-05-10 14:05:26 -0600

    commit tree aa9657143664d01a7fc87a3ead5b62a63527c7be

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,30 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
 end
